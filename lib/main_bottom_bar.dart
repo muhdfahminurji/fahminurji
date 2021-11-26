035970f9462b9c0f0dc6119f4500cfe8d3469574
Ming Choy <chaimingchoy@graduate.utm.my>
2021-11-26 15:48:31 +0800

    Bottom Bar Complete

--- lib/main_bottom_bar.dart
+++ lib/main_bottom_bar.dart
@@ -1,14 +1,87 @@
 import 'package:flutter/material.dart';
 
 class MainBottomBar extends StatelessWidget {
+  final state;
+
+  MainBottomBar({this.state});
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 180.0,
       child: Column(children: [
-        Text('Main Bottom Bar'),
-        Text('Main Bottom Bar'),
-        Text('Main Bottom Bar')
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Slider(
+              min: 0,
+              max: 255,
+              value: state.red,
+              onChanged: (value) => state.red = value.round(),
+            )),
+            if (state.allowcolor)
+              FloatingActionButton(
+                onPressed: () {
+                  state.red = 255;
+                  state.green = 0;
+                  state.blue = 0;
+                },
+                backgroundColor: Colors.red,
+                child: Text('${state.red}'),
+              )
+            else
+              Text('${state.red}')
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Slider(
+              min: 0,
+              max: 255,
+              value: state.green,
+              onChanged: (value) => state.green = value.round(),
+            )),
+            if (state.allowcolor)
+              FloatingActionButton(
+                onPressed: () {
+                  state.red = 0;
+                  state.green = 255;
+                  state.blue = 0;
+                },
+                backgroundColor: Colors.green,
+                child: Text('${state.green}'),
+              )
+            else
+              Text('${state.green}')
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Slider(
+              min: 0,
+              max: 255,
+              value: state.blue,
+              onChanged: (value) => state.blue = value.round(),
+            )),
+            if (state.allowcolor)
+              FloatingActionButton(
+                onPressed: () {
+                  state.red = 0;
+                  state.green = 0;
+                  state.blue = 255;
+                },
+                backgroundColor: Colors.blue,
+                child: Text('${state.blue}'),
+              )
+            else
+              Text('${state.blue}')
+          ],
+        ),
       ]),
     );
   }
