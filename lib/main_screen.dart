1da0b93678977d5704f35670f536301c341576ea
Ming Choy <chaimingchoy@graduate.utm.my>
2021-11-26 15:49:05 +0800

    Screen Complete

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -10,13 +10,67 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _size = 100;
+  int get size => _size;
+  set size(value) => setState(() => _size = value);
+
+  int _red = 0;
+  get red => _red;
+  set red(value) => setState(() => _red = value);
+
+  int _green = 0;
+  get green => _green;
+  set green(value) => setState(() => _green = value);
+
+  int _blue = 255;
+  get blue => _blue;
+  set blue(value) => setState(() => _blue = value);
+
+  bool _allowresize = true;
+  get allowresize => _allowresize;
+  set allowresize(value) => setState(() => _allowresize = value);
+
+  bool _allowcolor = true;
+  get allowcolor => _allowcolor;
+  set allowcolor(value) => setState(() => _allowcolor = value);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: MainAppBar(),
-        body: MainBody(),
-        bottomNavigationBar: MainBottomBar(),
+        appBar: MainAppBar(state: this),
+        body: MainBody(state: this),
+        bottomNavigationBar: MainBottomBar(state: this),
+        drawer: Drawer(
+          child: ListView(
+            itemExtent: 50.0,
+            padding: EdgeInsets.zero,
+            children: [
+              CheckboxListTile(
+                title: Text('Allow resize?'),
+                value: allowresize,
+                onChanged: (value) {
+                  if (value)
+                    allowresize = true;
+                  else
+                    allowresize = false;
+                },
+              ),
+              Divider(),
+              CheckboxListTile(
+                title: Text('Allow change primer color?'),
+                value: allowcolor,
+                onChanged: (value) {
+                  if (value)
+                    allowcolor = true;
+                  else
+                    allowcolor = false;
+                },
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
