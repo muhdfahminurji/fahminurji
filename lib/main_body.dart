3614e5cdab55d9ab2fa864898f8aee80f659c878
Ming Choy <chaimingchoy@graduate.utm.my>
2021-11-26 15:47:53 +0800

    Body Complete

--- lib/main_body.dart
+++ lib/main_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MainBody extends StatelessWidget {
+  final state;
+  MainBody({this.state});
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Main Body'),
+      child: Icon(
+        Icons.alarm,
+        size: state.size,
+        color: Color.fromRGBO(state.red, state.green, state.blue, 1),
+      ),
     );
   }
 }
